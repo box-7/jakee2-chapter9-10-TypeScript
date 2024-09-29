947ef57c03deea9172b43fbfda9c4b8a2620119a
box-7 <hayainada1981@yahoo.co.jp>
2024-09-30 07:09:13 +0900

    chapter9完了

---
+++ src/typeList.ts
@@ -0,0 +1,35 @@
+// Typescriptの基本の型
+
+// boolean
+let bool: boolean = true;
+
+// string
+let str: string = 'string';
+
+// Array
+let arr1: Array<number> = [1, 2, 3];
+let arr2: number[] = [1, 2, 3];
+
+// tuple
+let tuple: [number, string] = [1, 'string'];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = (): void => {
+  const test = 'TEST';
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj2: { id: number, name: string } = {id: 0, name: "aaa"};
+
+
+
